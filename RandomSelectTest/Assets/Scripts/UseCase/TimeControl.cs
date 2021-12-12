7a622b68ba156be6ea8c1929254551e7265fde0c
minhtet97 <minhtetnai97@gmail.com>
2021-12-13 01:35:10 +0900

    ランダンム
    Random

---
+++ RandomSelectTest/Assets/Scripts/UseCase/TimeControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimeControl : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 100f;
+
+    public float fastForwardMultipler = 5f;
+    public bool fastForward;
+
+    [SerializeField] Text countDownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownText.text = currentTime.ToString("0");
+
+        if(fastForward == true )
+        {
+            currentTime -= 2 * Time.deltaTime;
+        }
+    }
+}
---
+++ RandomSelectTest/Assets/Scripts/UseCase/ball.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float speed = 5f;        //player Speed
+    private float horizontalInput;  // player move
+    private float forwardInput;     // input forward
+
+    public bool isBoostedActived = false;  // boostspeed bool
+    [SerializeField]
+    private float BoostTime = 10.0f;   // boost Time
+    [SerializeField] 
+    private float BoostIncrease = 10.0f;  //boost Speed
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+    
+    private void OnCollisionEnter(Collision hit)
+    {
+        switch(hit.gameObject.tag)
+        {
+            case "BallSpeed":
+            if(isBoostedActived)
+            {
+                speed = speed * BoostIncrease;
+            }
+            //else
+            //{
+            //    speed = speed * 1.0f;
+            //}
+            if(!isBoostedActived)
+            {
+                isBoostedActived = true;
+                Invoke("EndBoost", BoostTime);
+            }
+            //speed = 20f;
+            //Destroy(gameObject,10);
+            break;
+
+            case "Multiple":
+            Destroy(hit.gameObject);
+            Instantiate(this.gameObject, transform.position, Quaternion.identity);
+            Destroy(gameObject,10);
+            break;
+
+            //この所あまり出来てない
+            case"TimeSpeed":
+            gameObject.GetComponent<TimeControl>().fastForward = false;
+            break;
+        }
+    }
+    private void EndBoost()
+    {
+        isBoostedActived = false;
+    }
+}
+
